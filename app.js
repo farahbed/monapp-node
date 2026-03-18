853d8800348d7b2f7096fec2f66247c2b4ff4545
farahbed <farah.bendriss.la@gmail.com>
2026-03-18 13:17:06 +0100

    fix docker push token

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello DevOps!" });
+  res.json({ message: "Hello DevOps girls!" });
 });
 
 app.listen(port, '0.0.0.0', () => {
