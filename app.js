423559b593446812dfa2f1ceb067f058db4fec48
farahbed <farah.bendriss.la@gmail.com>
2026-03-18 14:30:14 +0100

    update app message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello DevOps girls!" });
+  res.json({ message: "Hello DevOps we are learning!" });
 });
 
 app.listen(port, '0.0.0.0', () => {
